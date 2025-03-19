b53667148bad8b0cd7508eb32c8a14390d1120ef
Diana Milena Rodriguez Coy <dianarodriguezcoy@gmail.com>
2025-03-18 22:56:43 -0300

    Crear dos clases

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Dynamic;
+
+public class Cassette{
+
+    //ATRIBUTOS 
+    public string NombreAlbum {get; set;}
+    public string NombreArtista {get; set;}
+    public int  NumEdicion {get; set;}
+    public double Duracion {get; set;}
+
+    //METODOS
+
+    public void MostrarInformacionCassette()
+    {
+        Console.WriteLine($"Cassette:{NombreAlbum} - Artista:{NombreArtista}");
+        Console.WriteLine($"Duración:{Duracion}");
+    }
+    public void ReproducirCassette()
+    {
+        Console.WriteLine($"Estas Escuchando{NombreAlbum}");
+    }
+
+}
+
+public class Fanzine{
+    //ATRIBUTOS
+
+    public string Titulo {get;set;}
+    public string  NombreAutor {get; set;}
+    public int NumPaginas{get; set;}
+    public DateTime FechaPublicacion{get; set;}
+
+    //METODOS
+
+        public void MostrarInformacionFanzine()
+    {
+        Console.WriteLine($"Fanzine:{Titulo} - Autor:{NombreAutor}");
+        Console.WriteLine($"Fecha Publicacion:{FechaPublicacion}");
+        Console.WriteLine($"Número de páginas {NumPaginas}");
+    }
+        public void LeerFanzine()
+        {
+            Console.WriteLine($"Estas Leyendo el {Titulo}");
+        }
+
+        public void IrAPagina(int pagina)
+{
+    if (pagina > 0 && pagina <= NumPaginas)  
+    {
+        Console.WriteLine($"Estás en la página {pagina} de '{Titulo}'.");
+    }
+    else
+    {
+        Console.WriteLine($"¡Error! El fanzine solo tiene {NumPaginas} páginas. No existe la página {pagina}.");
+    }
+}
+
+    }
+
+
